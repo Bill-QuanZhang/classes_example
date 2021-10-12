c47e4ead778b948fb08944011c908598f7211b51
Bill Zhang <20030612zq@gmail.com>
2021-10-12 08:32:57 -0700

    create output_image

--- image_magic.py
+++ image_magic.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 # load the image (pumpkin)
 image = Image.open('./halloween-unsplash.jpg')
+output_image = Image.open('./halloween-unsplash.jpg')
 
 # Grab pixel information
 a_pixel = image.getpixel((0, 0))  # grab pixel (0, 0)
