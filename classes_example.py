42bef94a19916a536f3541b96a83d8b0c4c21080
Bill Zhang <20030612zq@gmail.com>
2021-10-21 11:22:17 -0700

    create a class; add 3 def (__init__, area, perimeter).

--- classes_example.py
+++ classes_example.py
@@ -1,3 +1,29 @@
 # Classes Example
 
 # Create some classes and projects
+
+class Shape:
+    """Represents a 2-dimensional polygon
+
+    Attributes:
+        num_sides: An integer count of the sides
+        side_length: A float of side length
+    """
+
+    def __init__(self):
+        """Creates a new shape with default value"""
+        self.num_sides = 4
+        self.side_length = 10.0
+
+    def area(self) -> float:
+        """Return the area of a square."""
+        return self.side_length ** 2
+
+    def perimeter(self) -> float:
+        """return the perimeter of a square"""
+        return self.num_sides * self.side_length
+
+
+some_shape = Shape()
+print(some_shape.num_sides)
+print(some_shape.perimeter())
